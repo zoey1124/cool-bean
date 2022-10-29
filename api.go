a0dcc90256b946847e43fc5f11a439794e597990
zoey1124 <zoey_1124@berkeley.edu>
2022-10-29 14:07:52 -0700

    complete draft for api

--- api.go
+++ api.go
@@ -1,24 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/cs161-staff/project2-starter-code/client"
 )
 
 func main() {
-	fmt.Println(os.Args[1])
-	if len(os.Args) < 2 {
-		fmt.Println("Missing parameter")
-		return
+	// Read input commands from a json file
+	jsonFile, err := os.Open("input.json")
+	if err != nil {
+		fmt.Println(err)
 	}
-	command := os.Args[1]
+	fmt.Println("Successfully Opened json file")
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var input map[string]string
+	json.Unmarshal([]byte(byteValue), &input)
+	command := input["command"]
+	username := input["username"]
+	password := input["password"]
+
+	// Parse command and call client functions
+	fmt.Println(command)
 	switch command {
 	case "InitUser":
-		user, _ := client.InitUser("Alice", "password")
-		fmt.Println(user.Username)
+		client.InitUser(username, password)
 	case "GetUser":
-		// ...
+		client.GetUser(username, password)
+	case "StoreFile":
+		user, _ := client.GetUser(username, password)
+		filename := input["filename"]
+		content := input["content"]
+		user.StoreFile(filename, []byte(content))
+	case "LoadFile":
+		user, _ := client.GetUser(username, password)
+		filename := input["filename"]
+		user.LoadFile(filename)
+	case "AppendFile":
+		user, _ := client.GetUser(username, password)
+		filename := input["filename"]
+		content := input["content"]
+		user.AppendToFile(filename, []byte(content))
+	case "CreatInvitation":
+		user, _ := client.GetUser(username, password)
+		filename := input["filename"]
+		recipientName := input["recipient_name"]
+		user.CreateInvitation(filename, recipientName)
+	case "AcceptInvitation":
+		user, _ := client.GetUser(username, password)
+		senderName := input["sender_name"]
+		invitationUUID := input["invitation_uuid"]
+		filename := input["filename"]
+		user.AcceptInvitation(senderName, invitationUUID, filename)
+	case "RevokeAccess":
+		user, _ := client.GetUser(username, password)
+		filename := input["filename"]
+		recipientName := input["recipient_name"]
+		user.RevokeAccess(filename, recipientName)
 	}
 }
