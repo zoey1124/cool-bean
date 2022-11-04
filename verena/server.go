4d4701f18ce26c794b59ae8f8d9126d683645a4f
zoey1124 <zoey_1124@berkeley.edu>
2022-11-04 10:03:23 -0700

    change input file passing in as argument

--- verena/server.go
+++ verena/server.go
@@ -1,61 +1,70 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "github.com/cs161-staff/project2-starter-code/client"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/cs161-staff/project2-starter-code/client"
 )
 
 func initUser(username string, password string) {
-    client.InitUser(username, password)
+	client.InitUser(username, password)
 }
 
-func getUser(username string, password string) (*client.User) {
-    user, _ := client.GetUser(username, password)
-    return user
+func getUser(username string, password string) *client.User {
+	user, _ := client.GetUser(username, password)
+	return user
 }
 
 func storeFile(user *client.User, filename string, content []byte) {
-    user.StoreFile(filename, content)
+	user.StoreFile(filename, content)
 }
 
 func loadFile(user *client.User, filename string) {
-    user.LoadFile(filename)
+	user.LoadFile(filename)
 }
 
 func appendFile(user *client.User, filename string, content []byte) {
-    user.AppendToFile(filename, content)
+	user.AppendToFile(filename, content)
 }
 
 func main() {
-    // Read input commands from a json file
-    jsonFile, err := os.Open("input.json")
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println("Successfully Opened json file")
-    defer jsonFile.Close()
-    byteValue, _ := ioutil.ReadAll(jsonFile)
-    var input map[string]string
-    json.Unmarshal([]byte(byteValue), &input)
-    command := input["command"]
-    username := input["username"]
-    password := input["password"]
+	// Read command line argument
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide input file")
+		return
+	}
+	input_file := os.Args[1]
 
-    // Parse command and call client functions
-    fmt.Println(command)
-    switch command {
-        case "InitUser":
-            initUser(username, password)
-        case "GetUser":
-            getUser(username, password)
-        case "StoreFile":
-            storeFile(getUser(username, password), input["filename"], []byte(input["content"]))
-        case "LoadFile":
-            loadFile(getUser(username, password), input["filename"])
-        case "AppendFile":
-            appendFile(getUser(username, password), input["filename"], []byte(input["content"]))
-    }
+	// Read input commands from a json file
+	jsonFile, err := os.Open(input_file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully Opened json file")
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var input map[string]string
+	json.Unmarshal([]byte(byteValue), &input)
+	command := input["command"]
+	username := input["username"]
+	password := input["password"]
+
+	// Parse command and call client functions
+	fmt.Println(command)
+	switch command {
+	case "InitUser":
+		initUser(username, password)
+	case "GetUser":
+		getUser(username, password)
+	case "StoreFile":
+		storeFile(getUser(username, password), input["filename"], []byte(input["content"]))
+	case "LoadFile":
+		loadFile(getUser(username, password), input["filename"])
+	case "AppendFile":
+		appendFile(getUser(username, password), input["filename"], []byte(input["content"]))
+	}
 }
