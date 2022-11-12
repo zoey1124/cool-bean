8b93781ab9d1f19be580d0bea19bf3fbbc36534d
victor <victor@noreply.github.com>
2022-11-12 13:54:47 -0800

    add test case for hash server

--- verena/client.py
+++ verena/client.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 
 USERNAME = "Alice"
 PASSWORD = "12345"
@@ -11,6 +12,21 @@ def create_command(command_name):
         "command": command_name
     }
 
+def run_commands(commands):
+    with open("input.json", "w") as f:
+        f.write(json.dumps(client_input))
+    os.system("go run server.go input.json")
+
+def test_hash_server():
+    r = requests.get("http://localhost:8090/get", json={"uuid": "1"})
+    print(r.text)
+
+    r = requests.post("http://localhost:8090/put", json={"uuid": "1", "entry":{"hash":"a", "version":0, "publicKey":"Alice"}})
+    print(r.text)
+
+    r = requests.get("http://localhost:8090/get", json={"uuid": "1"})
+    print(r.text)
+
 if __name__ == "__main__":
     client_input = {
         "inputs": [
@@ -18,6 +34,6 @@ if __name__ == "__main__":
             create_command("GetUser")
         ]
     }
-    with open("input.json", "w") as f:
-        f.write(json.dumps(client_input))
-    os.system("go run server.go input.json")
+    run_commands(client_input)
+
+    test_hash_server()
