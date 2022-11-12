a5afa5d26d957d075b85086cec04e8c194063e17
victor <victor@noreply.github.com>
2022-11-12 13:52:02 -0800

    change map to entry provided in verena

--- verena/hashServer.go
+++ verena/hashServer.go
@@ -4,7 +4,7 @@ package main
 // to run the server (from command line): go run hashServer.go
 // in a separate terminal:
 // to get a value: curl -X POST localhost:8090/get -H 'Content-Type: application/json' -d '{"uuid":"[VALUE]"}'
-// curl -X POST localhost:8090/put -H 'Content-Type: application/json' -d '{"uuid":"[VALUE]", "hash":"[HASH_VALUE]"}'
+// curl -X POST localhost:8090/put -H 'Content-Type: application/json' -d '{"uuid":"[VALUE]", "entry":{"hash:":"[HASH_VALUE]", "version":[VERSION], "publicKey:"[PUBLIC_KEY""}}'
 
 import (
     "fmt"
@@ -12,36 +12,61 @@ import (
     "net/http"
 )
 
-var m = make(map[string]string)
+type Entry struct {
+    Hash string `json:"hash"`
+    Version int `json:"version"`
+    PublicKey string `json:"publicKey"`
+}
 
-func parseRequestToJson(req *http.Request) map[string]string {
-    decoder := json.NewDecoder(req.Body)
-    var jsonData map[string]string
-    err := decoder.Decode(&jsonData)
+func (e Entry) String() string {
+    return fmt.Sprintf("[%s, %d, %s]", e.Hash, e.Version, e.PublicKey)
+}
+
+type GetRequest struct {
+    Uuid string `json:"uuid"`
+}
+
+type PutRequest struct {
+    Uuid string `json:"uuid"`
+    Entry Entry `json:"entry"`
+}
+
+var m = make(map[string]Entry)
+
+func get(w http.ResponseWriter, req *http.Request) {
+    var jsonData GetRequest
+    err := json.NewDecoder(req.Body).Decode(&jsonData)
     if err != nil {
         panic(err)
     }
-    return jsonData
-}
 
-func get(w http.ResponseWriter, req *http.Request) {
-    jsonData := parseRequestToJson(req)
-    uuid := jsonData["uuid"]
+    uuid := jsonData.Uuid
+    fmt.Fprintf(w, uuid + " " + m[uuid].String() + "\n")
 
-    fmt.Fprintf(w, uuid + " " + m[uuid] + "\n")
+    fmt.Println("Successful get for uuid: " + uuid)
 }
 
 func put(w http.ResponseWriter, req *http.Request) {
-    jsonData := parseRequestToJson(req)
-    uuid := jsonData["uuid"]
-    value := jsonData["hash"]
+    var jsonData PutRequest
+    err := json.NewDecoder(req.Body).Decode(&jsonData)
+    if err != nil {
+        panic(err)
+    }
 
-    m[uuid] = value
-    fmt.Fprintf(w, value + "put\n")
+    uuid := jsonData.Uuid
+    m[uuid] = jsonData.Entry
+    fmt.Fprintf(w, uuid + " " + jsonData.Entry.String() + " put\n")
+
+    fmt.Println("Successful put for uuid: " + uuid + " (entry: " + jsonData.Entry.String() + ")")
 }
 
 func main() {
-    m["1"] = "1"
+    // test case
+    m["1"] = Entry{
+        Hash: "a",
+        Version: 0,
+        PublicKey: "Alice",
+    }
 
     http.HandleFunc("/get", get)
     http.HandleFunc("/put", put)
